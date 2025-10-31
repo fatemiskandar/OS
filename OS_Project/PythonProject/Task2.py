87edae60ec73686776c8cef538b2c89d92b990fd
FatmaMuhammad <fatmamohammediskander55x@gmail.com>
2025-10-31 13:01:34 +0200

    edit

--- OS_Project/PythonProject/Task2.py
+++ OS_Project/PythonProject/Task2.py
@@ -3,13 +3,13 @@ from virtual_disk import VirtualDisk
 import fsConstant as fs
 
 class super_block:
-    def __init__(self, cluster_number, disk: VirtualDisk):
+    def __init__(self, disk: VirtualDisk):
         self.disk = disk
         self.disk.write(fs.super_cluster,bytes(fs.cluster_size))
 
     def read_super_block(self):
         return self.disk.read(fs.super_cluster)
     def write_super_block(self,data):
-        if len(data) != fs.super_cluster:
+        if len(data) != fs.cluster_size:
             raise ValueError("Super block size is not equal to cluster size")
-        self.disk.write(data,fs.super_cluster,data)
+        self.disk.write(fs.super_cluster,data)
